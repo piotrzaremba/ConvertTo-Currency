b6719ee068c65adbc5432636e1cb3216c60615bb
Piotr Zaremba <piotr.zaremba@intlfcstone.com>
2017-04-27 15:57:06 -0400

    Add Script.

---
+++ CurrencyConversion.psm1
@@ -0,0 +1,15 @@
+﻿function ConvertTo-Currency 
+{
+    param (
+        [double]$Amount,
+        [string]$Source,
+        [string]$Destination)
+ 
+    $Currency = New-WebServiceProxy -Uri http://www.webservicex.net/CurrencyConvertor.asmx?WSDL
+    $ConversionRate = $Currency.ConversionRate($Source, $Destination)
+    $DestinationAmount = $Amount * $ConversionRate
+
+    Write-Host "$Amount $Source convert to $DestinationAmount $Destination"
+} 
+
+Export-ModuleMember -function ConvertTo-Currency
